ec9b8e513d3e92ae66ca209a6afcbc7a5d7c1862
Snowpo <77101984+Snowpo@users.noreply.github.com>
2021-07-23 17:58:13 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,77 @@
+AFRAME.registerComponent("game-play", {
+  schema: {
+    elementId: { type: "string", default: "#ring1" },    
+  },
+  update: function () {
+    this.isCollided(this.data.elementId);
+  },
+
+  init: function () {
+    var duration = 120;
+    const timerEl = document.querySelector("#timer");
+    this.startTimer(duration, timerEl);
+  },
+
+  startTimer: function (duration, timerEl) {
+    var minutes;
+    var seconds;
+
+
+
+      setInterval(()=>{
+      if (duration >= 0) {
+        minutes = parseInt(duration / 60);
+        seconds = parseInt(duration % 60);
+
+        if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+          seconds = "0" + seconds;
+        }
+
+        timerEl.setAttribute("text", {
+          value: minutes + ":" + seconds,
+        });
+
+        duration -= 1;
+      } 
+      else {
+        this.gameover()       
+      }
+    },1000)
+  },
+  isCollided: function (elemntId) {
+    const element = document.querySelector(elemntId);
+    element.addEventListener("collide", (e) => {
+      if (elemntId.includes("#ring")) {
+        element.setAttribute("visible",false)
+        this.updatescore()
+        this.updatetargets()
+      } else {
+        this.gameover()
+      }
+    });
+  },
+  updatetargets:function(){
+    var element = document.querySelector('#targets')
+    var count=element.getAttribute('text').value
+    var currentTargets=parseInt(count)
+    currentTargets-=1
+    element.setAttribute('text',{value:currentTargets})
+  },
+  updatescore:function(){
+    var element = document.querySelector('#score')
+    var count=element.getAttribute('text').value
+    var currentScore=parseInt(count)
+
+    currentScore+=50
+    element.setAttribute('text',{value:currentScore})
+  },
+  gameover:function(){
+    var planeelement=document.querySelector('#plane_model')
+    var element=('#gameovertext')
+    element.setAttribute('visible',true)
+    planeelement.setAttribute('dynamic-body',{mass:1})
+  }
+});
